53cf70d6a7e4e2660381414fcbbfa428bc11d5a9
cpsdqs <cpsdqs@gmail.com>
2017-02-02 22:40:25 +0100

    Fix reset-transform breaking everything in environment without window

--- extra/reset-transform.js
+++ extra/reset-transform.js
@@ -1,4 +1,5 @@
-if (typeof window.CanvasRenderingContext2D.prototype.resetTransform !== 'function') {
+if (typeof window !== 'undefined' &&
+    typeof window.CanvasRenderingContext2D.prototype.resetTransform !== 'function') {
   window.CanvasRenderingContext2D.prototype.resetTransform = function () {
     this.setTransform(1, 0, 0, 1, 0, 0)
   }
